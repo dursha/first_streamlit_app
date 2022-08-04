22114f4094e23d0e4e47e0484c4c97c1c88efdde
Shanmugapriya <110605009+dursha@users.noreply.github.com>
2022-08-04 17:05:59 +0530

    Update streamlit_app.py
    Added Emojis

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,6 @@
 import streamlit
 streamlit.title('New Healthy Diner')
-streamlit.header('Breakfast Menu')
+streamlit.header('🥣 🥗 Breakfast Menu 🍞')
 streamlit.text('Idly,Vada')
 streamlit.text('Pongal,Vada')
 streamlit.text('Poori')
