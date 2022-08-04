88353f52ee7f39965cd8bdbb33bf9f92a7d0f10f
Shanmugapriya <110605009+dursha@users.noreply.github.com>
2022-08-04 17:09:20 +0530

    Update streamlit_app.py
    Included the code from the SF course

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,7 +1,10 @@
 import streamlit
 streamlit.title('New Healthy Diner')
-streamlit.header('🥣 🥗 Breakfast Menu 🍞')
-streamlit.text('Idly,Vada')
-streamlit.text('Pongal,Vada')
-streamlit.text('Poori')
-streamlit.text('Dosa')
+streamlit.header('Breakfast Menu')
+streamlit.text('🥣 Omega 3 & Blueberry Oatmeal')
+streamlit.text('🥗 Kale, Spinach & Rocket Smoothie')
+streamlit.text('🐔 Hard-Boiled Free-Range Egg')
+streamlit.text('🥑🍞 Avocado Toast')
+
+streamlit.header('🍌🥭 Build Your Own Fruit Smoothie 🥝🍇')
+
